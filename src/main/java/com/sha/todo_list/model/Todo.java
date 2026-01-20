b6f80f7b8ec99c596153b46a625cbfcf6a893376
SharmiLawrance <sharmilawrance22@gmail.com>
2026-01-20 22:30:38 +0530

    Created the required fields in Model package

--- src/main/java/com/sha/todo_list/model/Todo.java
+++ src/main/java/com/sha/todo_list/model/Todo.java
@@ -1,5 +1,46 @@
 package com.sha.todo_list.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long taskId;
+
+    @NotBlank(message = "Task name is required.")
+    private String taskName;
+
+    @Column(length = 100)
+    private String taskDescription;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus taskStatus = TaskStatus.NOT_STARTED;
+
+    private LocalDateTime taskCreatedAt;
+    private LocalDateTime taskUpdatedAt;
+    private LocalDateTime taskCompletedAt;
+    private LocalDateTime taskDueDate;
+
+    @PrePersist
+    public void onCreate() {
+        taskCreatedAt = LocalDateTime.now();
+        taskUpdatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        taskUpdatedAt = LocalDateTime.now();
+    }
 
 }
